1bfe2219ea6f8a4b86b24ab203f50205fa98798e
dylfrancis <dylfrancis505@gmail.com>
2026-02-24 15:52:18 -0500

    Build and host on github pages

--- projects/project-1/vite.config.js
+++ projects/project-1/vite.config.js
@@ -2,7 +2,10 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/visual-interfaces-data/project-1/',
   build: {
+    outDir: '../../docs/project-1',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
