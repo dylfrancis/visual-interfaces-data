954c5720d9d2031a258e5a67e7bcc4e46183f628
dylfrancis <dylfrancis505@gmail.com>
2026-02-24 15:56:47 -0500

    Add github action for building website

--- projects/project-1/vite.config.js
+++ projects/project-1/vite.config.js
@@ -4,8 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/visual-interfaces-data/project-1/',
   build: {
-    outDir: '../../docs/project-1',
-    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
